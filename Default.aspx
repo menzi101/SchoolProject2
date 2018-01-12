4e6e809cd07ec1cc503432cf97bd4c86c5623f4d
menzi101 <menzi.hlope@gmail.com>
2018-01-12 22:18:23 +0200

    First Commit

---
+++ Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Concert Booking</title>
+    <style type="text/css">
+        .style1
+        {
+            font-size: xx-large;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body bgcolor="#00ccff">
+    <form id="form1" runat="server">
+    <p class="style1">
+        Concert</p>
+    <p>
+        &nbsp;</p>
+    <p>
+        Tickets Remaining:
+        <asp:Label ID="lblTickets" runat="server" style="font-weight: 700"></asp:Label>
+    </p>
+    <p>
+        Food Choice:<br />
+        <asp:RadioButtonList ID="radFood" runat="server">
+            <asp:ListItem Selected="True">Beef</asp:ListItem>
+            <asp:ListItem>Chicken</asp:ListItem>
+            <asp:ListItem>Vegeterian</asp:ListItem>
+        </asp:RadioButtonList>
+    </p>
+    <p>
+        Seat:         <asp:TextBox ID="txtSeat" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Name:
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="btnBook" runat="server" Height="45px" Text="Book" 
+            Width="103px" onclick="btnBook_Click" />
+    </p>
+    <asp:Label ID="lblMessage" runat="server" ForeColor="#006600"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,85 @@
+﻿// Name: Menzi Hlope
+// STDNum: PT2010-0520
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    static string sConnect = "Provider = Microsoft.ACE.OleDb.12.0; Data Source = |DATADIRECTORY|concert.mdb";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            OleDbConnection odConnect = new OleDbConnection(sConnect);
+            odConnect.Open();
+
+            OleDbCommand odCommand = odConnect.CreateCommand();
+
+            odCommand.CommandText = "SELECT COUNT(*) AS [Total] FROM [Ticket]";
+
+            OleDbDataReader QRead = odCommand.ExecuteReader();
+            QRead.Read();
+
+            int iCount = (int)QRead["Total"];
+
+            int iTotal = 1000 - iCount;
+
+            lblTickets.Text = iTotal.ToString();
+            txtSeat.Focus();
+        }
+    }
+    protected void btnBook_Click(object sender, EventArgs e)
+    {
+        OleDbConnection odConnect = new OleDbConnection(sConnect);
+        odConnect.Open();
+
+        OleDbCommand odCommand = odConnect.CreateCommand();
+
+        odCommand.CommandText = "SELECT * FROM [Ticket] WHERE [SeatNum] = @seat";
+        odCommand.Parameters.Add("@seat", OleDbType.Integer).Value = txtSeat.Text;
+
+        OleDbDataReader QRead = odCommand.ExecuteReader();
+
+        if (QRead.HasRows == true)
+        {
+            lblMessage.Text = "This seat has already been taken. Please choose another one.";
+            lblMessage.ForeColor = System.Drawing.Color.Maroon;
+        }
+        else
+        {
+            QRead.Close();
+            odCommand.Parameters.Clear();
+
+            odCommand.CommandText = "INSERT INTO [Ticket] ([FoodChoice], [SeatNum], [Name]) VALUES (@food, @seat, @name)";
+            odCommand.Parameters.Add("@food", OleDbType.VarChar).Value = radFood.SelectedValue;
+            odCommand.Parameters.Add("@seat", OleDbType.Integer).Value = txtSeat.Text;
+            odCommand.Parameters.Add("@name", OleDbType.VarChar).Value = txtName.Text;
+
+            odCommand.ExecuteNonQuery();
+
+            odCommand.CommandText = "SELECT COUNT(*) AS [Total] FROM [Ticket]";
+
+            QRead = odCommand.ExecuteReader();
+            QRead.Read();
+
+            int iCount = (int)QRead["Total"];
+
+            int iTotal = 1000 - iCount;
+
+            lblTickets.Text = iTotal.ToString();
+            txtSeat.Focus();
+
+            lblMessage.Text = string.Format("Successfully booked {0} at seat {1}.", txtName.Text, txtSeat.Text);
+            lblMessage.ForeColor = System.Drawing.Color.DarkGreen;
+
+
+        }
+    }
+}
